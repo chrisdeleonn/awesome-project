74aeeb3ad967ddf9f5ac50174ab4e0dcce0d1570
Chris De Leon <cdeleon.ca91@gmail.com>
2021-08-17 14:20:49 -0400

    set up new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { Layout, Menu, Avatar } from 'antd';
+import Title from 'antd/lib/typography/Title'
+import SubMenu from 'antd/lib/menu/SubMenu';
+import Icon from '@ant-design/icons'
+
 import './App.css';
+
+
+const { Header, Footer, Sider, Content } = Layout
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Layout>
+      <Header style={{padding:10}}>
+        <Avatar style={{float: 'right'}} src='./among_us_avatar.png' />
+      <Title style={{color: 'white'}} level={3}>Chris</Title>
+      </Header>
+      <Layout>
+      <Sider>
+        <Menu defaultSelectedKeys={['Dashboard']}
+        mode="inline"
         >
-          Learn React
-        </a>
-      </header>
+          <Menu.Item key='Dashboard'>
+          Garage
+        </Menu.Item>
+        <SubMenu
+        title={
+          <span>
+            <Icon type='mail' />
+            <span>Profile</span>
+          </span>
+        }
+        >
+          <Menu.ItemGroup key='0' title='Log History'>
+            <Menu.Item key='1'>Tires</Menu.Item>
+            <Menu.Item key='2'>Oil</Menu.Item>
+            <Menu.Item key='3'>Brakes</Menu.Item>
+            <Menu.Item key='4'>Battery</Menu.Item>
+            <Menu.Item key='5'>Air Filter</Menu.Item>
+
+         </Menu.ItemGroup>
+        </SubMenu>
+        </Menu>
+      </Sider>
+      <Layout>
+      <Content>Content</Content>
+      <Footer>Footer</Footer>
+      </Layout>
+      </Layout>
+      </Layout>
     </div>
   );
 }
